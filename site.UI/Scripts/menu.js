03025edea287808b792d90605367c9e35cfda723
abderrafaa.gahbiche <abderrafaa.gahbiche@dlink.com>
2017-12-14 17:41:21 +0100

    Menu for mobile version

---
+++ site.UI/Scripts/menu.js
@@ -0,0 +1,39 @@
+﻿$(document).ready(function () {
+    var Menu = {
+        el: {
+            ham: $('.menu-header'),
+            menuTop: $('.menu-top'),
+            menuMiddle: $('.menu-middle'),
+            menuBottom: $('.menu-bottom')
+        },
+
+        init: function () {
+            Menu.bindUIactions();
+        },
+
+        bindUIactions: function () {
+            Menu.el.ham
+                .on(
+                  'click',
+                function (event) {
+                    Menu.activateMenu(event);
+                    event.preventDefault();
+                    if ($('.menu-list').position().left == 0) {
+                        $('.menu-list').animate({ left: '-315px' }, 'slow');
+                    }
+                    else {
+                        $('.menu-list').animate({ left: '0' }, 'slow');
+                    }
+                }
+            );
+        },
+
+        activateMenu: function () {
+            Menu.el.menuTop.toggleClass('menu-top-click');
+            Menu.el.menuMiddle.toggleClass('menu-middle-click');
+            Menu.el.menuBottom.toggleClass('menu-bottom-click');
+        }
+    };
+
+    Menu.init();
+});
